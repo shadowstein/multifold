baab701de28f6f43cb040f8e28c8efb7924e6fac
JB Quitoriano <screamonster@yahoo.com>
2011-04-08 15:56:43 +0800

    added articles folder for the storage of all articles regarding mfl, edited the news page, added the article header

--- pages/news.php
+++ pages/news.php
@@ -1,18 +1,23 @@
 <div id="content">
+  <h2>Articles</h2>	
+  <ul>
+  	<li>
+  		<a href="articles/Virtualization Why You Shouldn't Jump In Just Yet.pdf">03.02.2011 - Virtualization: Why You Shouldn't Jump In Just Yet</a>
+  	</li>
+  </ul>
   <h2>Latest Headlines</h2>
-
   
-  <h3><a name="news4"></a>MFL starts beta testing of its new Prize Claim software</h3>
+  <h4><a name="news4"></a>MFL starts beta testing of its new Prize Claim software</h4>
   <p>MFL engineers has began testing of its new prize claim software. The software claims to boost claiming procedures and processes making operations streamlined. The software will authenticate winner's claim stubs/tickets and keep a record of their profile. This new development will put lotto and sweepstakes operation in the digital map where they will be implementing electronic records management. </p>
   <div id="headlinesCFrom">05 February 2010 09:30AM</div>
 
   <br>      
-  <h3><a name="news2"></a>MFL CCO meets with Palau President</h3>
+  <h4><a name="news2"></a>MFL CCO meets with Palau President</h4>
   <p>MFL Inc. Chief Channels Officer, Karl Favila embarked on a 2 day trip to the wonderful island of Palau to have an exploratory discussion with Palau president Johnson Toribiong. Mr. Favila stated 'Palau has many things to offer to the world and most especially the value of its environment.'  The small nation of Palau which consists of a small population (20,000) is very well known for hosting the Survivor show and being the 'place to be' in Micronesia. Mr. Favila also stated ' meeting with President Toribiong was just such an honor. He is a very down to earth man and is very visionary, I sure did learn a lot about history from him during my brief visit. The president even kidded that the Guantanamo Bay prisoners that the country absorbed from the United States were as handsome as me. Foreign private sectors and public sectors have a lot to offer to Palau and vice versa, we just have to do the right things in the right places in order to assist Palau in becoming one of Asia's booming countries.' </p>
   <div id="headlinesCFrom">22 January 2010 01:10PM</div>
 
 
-  <h3><a name="news1"></a>MFL wins PGH project for its Cash Collection System project</h3>
+  <h4><a name="news1"></a>MFL wins PGH project for its Cash Collection System project</h4>
   <p>UP-PGH awards MFL, Inc. for its POS system project for its Cash Services Department.
   After complying with the requirements of PGH and a successful bidding, MFL, Inc. is declared winner of the PGH POS project will be underway in a few weeks. MFL, Inc. Head of Marketing, Maebel Irlanda states that this project will be the start of a good long-lasting business relationship with the Philippines' premier government hospital and premier university. 
   </p>
